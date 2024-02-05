5f6889b8c5146e8c963a0e44eb29e7a09c161734
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-05 20:04:50 +0530

    2nd program

--- Hello_world/firstJavaProgram.java
+++ Hello_world/firstJavaProgram.java
@@ -1,5 +1,8 @@
 public class firstJavaProgram {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int n1 = 1, n2 = 5, sum; 
+        sum = n1 + n2;
+        System.err.println(sum);
     }
 }
