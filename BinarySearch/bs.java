ad62e388e7491ad582ec7fbc060814d4d165b5e4
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-05 23:23:38 +0530

    Binary Search

---
+++ BinarySearch/bs.java
@@ -0,0 +1,40 @@
+public class bs {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if the target is present at the middle
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            // If the target is greater, ignore the left half
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            // If the target is smaller, ignore the right half
+            else {
+                right = mid - 1;
+            }
+        }
+
+        // If we reach here, the target is not present in the array
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 9;
+
+        int result = binarySearch(sortedArray, target);
+
+        if (result != -1) {
+            System.out.println("\nElement found at index: \n" + result);
+        } else {
+            System.out.println("\nElement not found in the array.");
+        }
+    }
+}
