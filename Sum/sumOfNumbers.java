c27f472d7b73b245784a0b53ef10506cf237268d
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-05 20:10:22 +0530

    sum

---
+++ Sum/sumOfNumbers.java
@@ -0,0 +1,8 @@
+public class sumOfNumbers{
+    public static void main(String[] args) {
+        int n1 = 1, n2 = 4, sum;
+        sum = n1 + n2;
+        System.out.println(sum);
+        System.out.println(n1-n2);
+    }
+}
